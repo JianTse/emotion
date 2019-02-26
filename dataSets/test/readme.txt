a19514cd74c3c58e5b41c7086499316ac47940fd
xiej <280144557@qq.com>
2019-02-26 09:46:38 +0800

    情绪识别训练、测试数据集，以及网络结构，训练参数

---
+++ dataSets/test/readme.txt
@@ -0,0 +1,8 @@
+
+mtcnn_detectInfo存放的是label信息
+img_idx, x, y, w, h, ldmark_x1, ldmark_y1,....ldmark_x5, ldmark_y5,label
+其中，如果w,h=0说明这一张图片不参与统计
+
+
+
+files中存放的是与img_idx对应的图片
---
+++ model/train2.sh
@@ -0,0 +1,11 @@
+wdir=`pwd`
+# log
+DATE=$(date "+%Y-%m-%d-%H-%M-%S")
+if [ ! -d "./log" ];then
+    mkdir log
+fi
+
+logpath=${wdir}/log/${DATE}.log
+#nohup python -u ${script} > ${logpath} 2>&1  & 
+nohup ../build/tools/caffe train --solver=./emo2-solver.prototxt -gpu=0,1 > ${logpath} 2>&1  & 
+tail -f ${logpath}
